a08fcfd23fe82a02ce2f44259daa35d817ba921a
William McFall <wmcfall2@gmail.com>
2019-05-08 15:44:57 -0400

    removed excess rendering from cards

--- react-matching-game/src/components/FriendCard/index.js
+++ react-matching-game/src/components/FriendCard/index.js
@@ -7,19 +7,6 @@ function FriendCard(props) {
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
       <span onClick={() => props.removeFriend(props.id)} className="remove">
         𝘅
       </span>
